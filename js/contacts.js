48c2e3ca5bac2d12a76c7bf9760bf2f08b2ea3fb
Unknown <JeanMarc.Hebert1996@gmail.com>
2019-03-07 07:43:00 -0500

    Version finale
    Version finale projet 1

---
+++ js/contacts.js
@@ -0,0 +1,5 @@
+//Jean-Marc Hebert et Mohamed LADJADJAT
+var contacts=[{"prenom":"Jean-Marc","nom":"Hebert","cle":"ClePublique1"},
+	{"prenom":"Mohamed","nom":"Ladjadjat","cle":"ClePublique2"},
+	{"prenom":"Wojciech","nom":"Fraczak","cle":"ClePublique3"}
+]
