e178d43ec806f9cf37138e78cfd02fbb7e5bf167
Fadi Mohamed <45546270+fadi-hiroki@users.noreply.github.com>
2019-03-15 20:26:20 -0400

    Add files via upload

---
+++ js/contacts.js
@@ -0,0 +1,6 @@
+//Jean-Marc Hebert et Mohamed LADJADJAT
+var contacts=[
+    {"cle":"ClePublique1","prenom":"Jean-Marc","nom":"Hebert"},
+	{"cle":"ClePublique2","prenom":"Mohamed","nom":"Ladjadjat"},
+	{"cle":"ClePublique3","prenom":"Wojciech","nom":"Fraczak"}
+]
