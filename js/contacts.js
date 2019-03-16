06d7c23488ff77c94e8c780839492ed02bd240af
Fadi Mohamed <45546270+fadi-hiroki@users.noreply.github.com>
2019-03-16 01:25:49 +0100

    Delete contacts.js

--- js/contacts.js
+++
@@ -1,5 +0,0 @@
-//Jean-Marc Hebert et Mohamed LADJADJAT
-var contacts=[{"prenom":"Jean-Marc","nom":"Hebert","cle":"ClePublique1"},
-	{"prenom":"Mohamed","nom":"Ladjadjat","cle":"ClePublique2"},
-	{"prenom":"Wojciech","nom":"Fraczak","cle":"ClePublique3"}
-]
